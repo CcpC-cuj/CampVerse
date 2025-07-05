5511982df75e6229049a028f701fb83235347015
apurbasbjk30 <apurba131223@gmail.com>
2025-07-05 10:56:50 +0530

    update

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -1,28 +1,11 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', 'react-hooks'],
-  rules: {
-    'react/react-in-jsx-scope': 'off', // not needed with React 17+
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-};
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+]);
